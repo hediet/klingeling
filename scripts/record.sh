aa345736c2ae3a96a48d600ca927091f185fa05d
phiresky <phireskyde+git@gmail.com>
2019-06-03 19:13:41 +0200

    record script

---
+++ scripts/record.sh
@@ -0,0 +1,11 @@
+# create a ramdisk and record from webcam to MP4 DASH segments, keeping a history of 100 segments of 3 seconds each
+
+mkdir -p ramdisk
+mountpoint ramdisk || sudo mount -t tmpfs none ramdisk
+rm ramdisk/*
+/usr/bin/ffmpeg -f v4l2 -framerate 10 -video_size 1280x720 \
+    -i /dev/video0 \
+    -filter:v "transpose=2,crop=413:710:207:531,drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:text='%{localtime\:%X}':fontsize=60:fontcolor=white@0.8: x=7: y=7" \
+    -c:v libx264 -preset ultrafast -tune zerolatency -flags +cgop -g $((3*10)) -pix_fmt yuv420p -b:v 400k \
+    -seg_duration 3 -use_template 1 -use_timeline 0 -window_size 100 -streaming 1 -f dash \
+    ramdisk/manifest-stream0.mpd
